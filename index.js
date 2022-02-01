e00205d93f6fd8816d46ddf6ebea7c05c7fd2b08
mr.max <92267106+prasadkhairnar@users.noreply.github.com>
2022-02-01 17:13:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ function search(){
   
   const li=document.getElementsByTagName('li');
   const key = input.value;
+   const rev=key.toLowerCase();
   for(i=0;i<li.length;i++){
-    if(li[i].innerText.toLowerCase().includes(key)){
+    if(li[i].innerText.toLowerCase().includes(rev)){
 
         li[i].style.display="";
     }else{
